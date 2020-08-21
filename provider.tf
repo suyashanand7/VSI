7dada036b09eed3005e5f7cd86edb2a3cf0723f1
Suyash Anand <38377159+suyashanand7@users.noreply.github.com>
2020-08-21 12:26:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 variable "softlayer_username" {
-  description = "Enter your IBM Infrastructure (SoftLayer) full username, you can get this using: https://control.bluemix.net/account/user/profile"
+  description = "vijaylakshmi"
 }
 
 variable "softlayer_api_key" {
-  description = "Enter your IBM Infrastructure (SoftLayer) API key, you can get this using: https://control.bluemix.net/account/user/profile"
+  description = "1251e62e5fb5cdc028c094e4cbd455681ede1eeaf1955d1631bc4466e474f2c7"
 }
 
 variable "ibmcloud_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
+  description = "WwgSeCY0-VWRKtug6cRBtayYjBHTR2lNyryWvfoIMtRr"
 }
 
 provider "ibm" {
