3fdd4ab99d6bc5cb3aa647c2921fa4d279ebf564
Suyash Anand <38377159+suyashanand7@users.noreply.github.com>
2020-08-24 13:13:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,20 @@ resource "ibm_compute_vm_instance" "vm1" {
   disks                = [25]
   local_disk           = false
 }
+variable "softlayer_username" {
+  description = "vijaylakshmi"
+}
+
+variable "softlayer_api_key" {
+  description = "1251e62e5fb5cdc028c094e4cbd455681ede1eeaf1955d1631bc4466e474f2c7"
+}
+
+variable "ibmcloud_api_key" {
+  description = "WwgSeCY0-VWRKtug6cRBtayYjBHTR2lNyryWvfoIMtRr"
+}
+
+provider "ibm" {
+  softlayer_username = var.softlayer_username
+  softlayer_api_key  = var.softlayer_api_key
+  ibmcloud_api_key   = var.ibmcloud_api_key
+}
